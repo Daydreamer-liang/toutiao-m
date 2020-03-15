54ac524883c5b200af2ed5b38efdf486e028be26
zhuxiangliang <916447016@qq.com>
2020-03-15 11:58:50 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'vant/lib/index.css'
 
 import Vant from 'vant'// 引入vant组件
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+
 Vue.use(Vant)// 全局注册vant
 
 Vue.config.productionTip = false
