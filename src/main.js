a18225b7c967bfadd55c8db920d151629f66bc9c
zhuxiangliang <916447016@qq.com>
2020-03-15 11:07:58 +0800

    注册使用移动端-vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
+
+import Vant from 'vant'// 引入vant组件
+Vue.use(Vant)// 全局注册vant
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="home">首页</div>
+  <div class="home">
+    首页
+    <van-radio-group v-model="radio">
+      <van-radio name="1">单选框 1</van-radio>
+      <van-radio name="2">单选框 2</van-radio>
+    </van-radio-group>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
