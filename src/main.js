9107887969320296e91fa08aded69b60d33e969e
zhuxiangliang <916447016@qq.com>
2020-03-16 22:27:46 +0800

    拦截登录-权限模块

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/permission'// 引入权限模块
 
 import Vant from 'vant'// 引入vant组件
 import 'vant/lib/index.css'
