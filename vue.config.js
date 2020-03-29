a5d234afc13e2b85d98a34010ff33ffca33d69fd
zhuxiangliang <916447016@qq.com>
2020-03-29 19:21:48 +0800

    打包项目

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,15 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // 打包 config 是vue-cli中所有的webpack配置
+    // 判断是开发环境 还是生产环境
+    if (process.env.NODE_ENV === 'production') {
+      // 只需要在生产环境中 ，把所有console 删除 在打包，此时不是删除源代码的，而是打包之后的console
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'// 打包后引用地址，不然访问不了
 
 }
